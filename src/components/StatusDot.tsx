d6dfab8270faa633136d25ca2a040c8568a83a6b
christian <c.schmitz1986@googlemail.com>
2026-05-02 08:53:34 +0200

    feat: StatusDot Puls-Indikator Komponente

---
+++ src/components/StatusDot.tsx
@@ -0,0 +1,15 @@
+interface StatusDotProps {
+  active: boolean
+}
+
+export function StatusDot({ active }: StatusDotProps) {
+  if (active) {
+    return (
+      <span className="relative flex h-3 w-3">
+        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-500 opacity-75" />
+        <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500" />
+      </span>
+    )
+  }
+  return <span className="inline-flex rounded-full h-3 w-3 bg-slate-600" />
+}
